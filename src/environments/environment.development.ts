282342b013cd3abbadf7e3723d841c2539fa3bbc
AlejandroAn12 <negocios1219@gmail.com>
2025-04-01 14:37:17 -0500

    implement:Components:privacy_policy-terms-1

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    API_URL: 'http://localhost:3001/api/v1'
-    // API_URL: 'https://posdekb-production.up.railway.app/api/v1',
+    // API_URL: 'http://localhost:3001/api/v1'
+    API_URL: 'https://posdekb-production.up.railway.app/api/v1',
     // API_URL: 'http://192.168.0.100:3001/api/v1'
 };
